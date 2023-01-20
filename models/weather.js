16f89ab728ea6edabf1d4cc6971ca58159d039bb
jrh <joshuaharris1103@gmail.com>
2023-01-19 19:16:35 -0500

    working on getting array of index to be presented

--- models/weather.js
+++ models/weather.js
@@ -6,17 +6,14 @@ const mongoose = require('mongoose') // import mongoose
 // we'll destructure the Schema and model functions from mongoose
 const { Schema, model } = mongoose
 
-// weather schema
+// make weather schema
 const weatherSchema = new Schema({
     name: String,
     humidity: String,
-    needUmbrella: Boolean
+    needUmbrella: Boolean,
 })
 
-// make the eather model
-// the model method takes two arguments
-// the eather is what we call our model
-// the second is the schema used to build the model
+// make the Weather model
 const Weather = model('Weather', weatherSchema)
 
 //////////////////////////
